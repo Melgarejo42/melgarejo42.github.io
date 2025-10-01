56aaf4fb83714fb53a8682039939c53d207da85c
Melgarejo42 <150817243+Melgarejo42@users.noreply.github.com>
2025-10-01 15:26:03 -0300

    Commit

--- trainer.js
+++ trainer.js
@@ -1,26 +1,111 @@
+/* 
+const product = {
+    name: 'socks',
+    price: 1090,
+};
 
-
-// Converter Fahrenheit para Celsius
-
-let celsius = prompt("Enter a temperature in Celsius")
-
-function convertToFahrenheit(celsius) {
-    return celsius * 9 / 5 + 32;
+function log() {
+    console.log(user);
 }
 
-let fahrenheit = convertToFahrenheit(parseFloat(celsius));
-
-console.log(`${celsius} degrees Celsius is equal to ${fahrenheit} degrees Fahrenheit`);
-
-
-let fahrenheit = prompt("Enter a temperature in Fahrenheit");
-
-function convertToCelsius(fahrenheit) {
-    return (fahrenheit - 32 * 5 / 9).toFixed(2);
+let user = {
+    name: 'Bruno',
+    age: 33,
+    height: 180,
+    salary: 5000,
+    hairColor: 'black',
+    birthYear: 1990,
 }
 
-let celsius = convertToCelsius(parseFloat(fahrenheit));
+function rename() {
+    user.name = prompt("What's your name");
+} */
 
-console.log(`${fahrenheit} degrees Fahrenheit is equal to ${celsius} degrees Celsius`);
+
+const product1 = {
+    name: 'cheap basketball',
+    price: 1000,
+}
+
+const product2 = {
+    name: 'Wilson basketball',
+    price: 2095,
+}
+
+product1['delivery-time'] = '3 days';
+
+console.log(product1.price + 500);
+
+function comparePrice(product1, product2) {
+    if (product1.price < product2.price) {
+        return product1;
+    } else {
+        return product2;
+    }
+}
+
+console.log(comparePrice(product1, product2));
+
+
+// 1. Print numbers from 1 to 10
+
+/* for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+ */
+// 2. Calculate the sum of the first 10 natural numbers
+
+/* var n = 10;
+var total = 0;
+
+for (var i = 1; i <= n; i++) {
+    total += i;
+}
+ */
+
+// 3. Print multiplication table
+/* 
+let n = 5;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(n + " x " + i + " = " + (n * i))
+} */
+
+// 4. Calculate factorial (Write a program to calculate the factorial of a given number. Ex: 5! = 5x4x3x2x1)
+
+/* let n = parseFloat(prompt("Enter a number:"));
+let factorial = 1;
+
+for (let i = n; i >= 1; i--) {
+    factorial *= i;
+}
+
+console.log("Factorial of", n, ":", factorial);
+
+ */
+
+// 5. Print even numbers from 1 to 20
+
+/* for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+} */
+
+// 6. Print odd numbers from 1 to 20
+
+/* for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+} */
+
+
+
+// 6. Print Fibonacci series
+
+
+
+// 7. Count vowels in a string
 
 
